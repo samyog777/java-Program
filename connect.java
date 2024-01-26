791a6c30301c3c10b3069f398371c9e01ee6c010
samyog777 <samyogkoirala18@gmail.com>
2024-01-26 12:04:14 +0545

    java program

---
+++ connect.java
@@ -0,0 +1,18 @@
+package final_project;
+import java.sql.*;
+
+public class connect {
+
+	public static void main(String[] args) {
+
+		String url ="";
+		String userName = "root";
+		String password = "";
+		
+		try {
+			Connection conn = DriveManager.getConnection(url,username,password);
+			Statement stm = 
+		}
+	}
+
+}
